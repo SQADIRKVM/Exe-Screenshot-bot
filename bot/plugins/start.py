e0c4e611bc257344849b96ae779d500a404c5d93
S.Qadir <70005344+SQADIRKVM@users.noreply.github.com>
2021-12-25 16:05:07 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -12,7 +12,7 @@ async def start(c, m, cb=False):
     mention = '[SQADIR](https://t.me/EDU_XOM)'
     try:
         owner = await c.get_users(owner_id)
-        username = owner.username if owner.username else 'Ns_AnoNymous'
+        username = owner.username if owner.username else 'SQADIRKVM'
         mention = owner.mention(style="md")
     except Exception as e:
         print(e)
